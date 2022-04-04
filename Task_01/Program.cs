69f6a127d5776a01a5b71c43cfc4c9e58e58d7d7
darlyncarmenate <darlyncarmenate@gmail.com>
2022-04-04 11:34:32 +0200

    Сколько положительных чисел введено

---
+++ Task_01/Program.cs
@@ -0,0 +1,21 @@
+﻿// Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+int Prompt(string message)
+{
+	Console.Write(message);
+	string strValue = Console.ReadLine();
+	int value = int.Parse (strValue);
+	return value;
+}
+
+int m = Prompt("Сколько чисел Вы хотите ввести? ");
+int count = 0;
+
+for(int i = 0; i < m; i++)
+{
+	int a = Prompt($"Введите число {i + 1}: ");
+	if(a > 0) count++;
+}
+
+Console.WriteLine($"Вы ввели {count} положительных числа");
